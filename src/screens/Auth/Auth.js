5870d1bdc1b92cb5682a45718a5ee4fbfbd6319f
tassoeb <tasso_barbosa@yahoo.com.br>
2019-07-10 16:36:50 -0300

    Icones e Navigation adicionados

---
+++ src/screens/Auth/Auth.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {View, Text, Button} from 'react-native';
+
+//import startMainTabs from '../MainTabs/startMainTabs';
+
+class AuthScreen extends Component {
+
+loginHandler = () => {
+  console.log("Bem vindo")
+}
+
+  render(){
+    return(
+      <View>
+        <Text>AuthScreen</Text>
+        <Button title="login" onPress={this.loginHandler} />
+      </View>
+    );
+  }
+}
+
+export default AuthScreen;
