e66b8bc66059c63cbbe5976ad838a48d85bb5fc8
tassoeb <tasso_barbosa@yahoo.com.br>
2019-07-24 10:44:04 -0300

    Example chart imported correctly

--- src/screens/Care/Care.js
+++ src/screens/Care/Care.js
@@ -7,6 +7,15 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from 'react-native-chart-kit'
+
 import CareButton from '../../Components/UI/CareButton';
 import Icon from "react-native-vector-icons/Ionicons";
 
@@ -92,6 +101,46 @@ class CareScreen extends Component {
         <CareButton name='Banho e Tosa' iconName='md-cut' onPress={this.goToPetshop}/>
         <CareButton name='Veterinário' iconName='md-medkit' onPress={this.goToVeterinario}/>
         <CareButton name='Cartão de vacinas' iconName='md-pulse' onPress={this.goToVacinas}/>
+
+        <View>
+          <Text>
+            Bezier Line Chart
+          </Text>
+          <LineChart
+            data={{
+              labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+              datasets: [{
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100
+                ]
+              }]
+            }}
+            width={Dimensions.get('window').width} // from react-native
+            height={220}
+            yAxisLabel={'$'}
+            chartConfig={{
+              backgroundColor: '#e26a00',
+              backgroundGradientFrom: '#fb8c00',
+              backgroundGradientTo: '#ffa726',
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16
+              }
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
+              borderRadius: 16
+            }}
+          />
+        </View>
+
       </View>
     );
   }
