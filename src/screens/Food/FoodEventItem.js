2a3ab2ae5e6340ed36b74b2217ba86d199d447c6
tassoeb <tasso_barbosa@yahoo.com.br>
2019-07-31 18:23:11 -0300

    FoodItem stylized

--- src/screens/Food/FoodEventItem.js
+++ src/screens/Food/FoodEventItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
+import CommonStyles from '../../Stylesheets/Common';
+
 const foodEventItem = (props) => (
   <TouchableOpacity onPress={props.onItemPressed}>
     <View style={styles.listItem}>
-      <View>
-        <Text style={styles.textItem}>{props.mealName}</Text>
+      <View style={styles.itemField}>
+        <Text style={styles.textTitle}>{props.mealName}</Text>
+      </View>
+      <View style={styles.itemField}>
         <Text style={styles.textItem}>{props.mealTime}</Text>
+      </View>
+      <View style={styles.itemField}>
         <Text style={styles.textItem}>{props.mealPortion}</Text>
+        <Text style={styles.textTitle}>porções</Text>
       </View>
     </View>
   </TouchableOpacity>
@@ -16,19 +23,28 @@ const foodEventItem = (props) => (
 const styles = StyleSheet.create({
     listItem: {
         width: "100%",
+        height: 80,
         padding: 10,
         marginBottom: 10,
         backgroundColor: "white",
         flexDirection: "row",
-        justifyContent: "space-between",
+        //justifyContent: "space-between",
         alignItems: "center",
         borderRadius: 5,
         borderWidth: 0.5,
         borderColor: '#d6d7da',
     },
+    itemField: {
+      width: "33%",
+    },
     textItem: {
+      fontSize: 26,
+      fontWeight: 'bold',
+      color: CommonStyles.mainColor,
+    },
+    textTitle: {
       fontSize: 18,
-      fontWeight: 'bold'
+      fontWeight: 'bold',
     }
 });
 
