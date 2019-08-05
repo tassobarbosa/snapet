36d96b3613af0925ce77f235b2b93203814e657b
tassoeb <tasso_barbosa@yahoo.com.br>
2019-08-05 15:22:43 -0300

    Rendering image and name at Home

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
-import {View, Text, Image} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet
+} from 'react-native';
 import { serverUrl } from '../../Config/Settings.js'
-//import startMainTabs from '../MainTabs/startMainTabs';
+import CommonStyles from '../../Stylesheets/Common';
 
 class HomeScreen extends Component {
   constructor(props){
@@ -10,11 +15,13 @@ class HomeScreen extends Component {
   }
 
   state = {
-    encodedData: ''
+    encodedData: '',
+    petName: ''
   }
 
   componentDidMount() {
       this.getPetPhoto();
+      this.getPetName();
   }
 
   getPetPhoto = () => {
@@ -36,6 +43,24 @@ class HomeScreen extends Component {
     })
   }
 
+  getPetName = () => {
+    fetch(serverUrl+"petdata.json")
+    .then(res => res.json())
+    .then(parsedRes => {
+      console.log(parsedRes)
+      const dataEvents = [];
+      for(let key in parsedRes){
+        dataEvents.push({
+          ...parsedRes[key],
+        })
+      }
+       this.setState({
+         petName: dataEvents[0].chosenPetName,
+     });
+
+    })
+  }
+
   onNavigatorEvent = event => {
     if(event.type === "NavBarButtonPress"){
       if(event.id === "sideDrawerToggle"){
@@ -48,14 +73,48 @@ class HomeScreen extends Component {
   render(){
 
     return(
-      <View>
-        <Image
-            style={{width: 66, height: 58}}
-            source={{uri: `data:image/gif;base64,${this.state.encodedData}`}}
-          />
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+              style={styles.imagePet}
+              source={{uri: `data:image/gif;base64,${this.state.encodedData}`}}
+            />
+        </View>
+        <Text style={styles.petNameStyle}>{this.state.petName}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex:1,
+      backgroundColor: CommonStyles.screenBackgroundColor,
+      paddingTop: "15%",
+      alignItems: 'center'
+
+    },
+    imageContainer:{
+      height: 255,
+      width: 255,
+      borderRadius: 125,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderWidth: 5,
+    },
+    imagePet: {
+      height: 250,
+      width: 250,
+      borderRadius: 125
+    },
+    petNameStyle:{
+      fontSize: 30,
+      paddingTop: 20,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      textAlignVertical: 'center',
+      color: CommonStyles.mainColor,
+    }
+});
 
 export default HomeScreen;
